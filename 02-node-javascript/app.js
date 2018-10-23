6b241887c7c9d405a374455a848be8adf2db9059
Vasant Hugar <vasant.hugar@indpro.se>
2018-10-23 18:51:05 +0530

    Fixed issues from 02-node-javascript\

--- 02-node-javascript/app.js
+++ 02-node-javascript/app.js
@@ -1,12 +1,12 @@
 var http = require('http');
-var modele1 = require('./modele1'); 
+var module1 = require('./module1'); 
 
 function onRequest(request, response) {
-    response.writehead(200, { 'Content-Type': 'plain/text' });
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Hello, This is my second application');
-    response.write(modele1.writehead);
-    modele1.myFunction;
+    response.write(module1.myString);
+    module1.myFunction();
     response.end();
 }
 
-http.createServer().listen(8000);
+http.createServer(onRequest).listen(8000);
