ec59158690f7b0ebc1bee11443aeef9915edef0b
Vasant Hugar <vasant.hugar@indpro.se>
2018-10-31 15:33:37 +0530

    Installed npm

--- 05-express-first-app/routes/users.js
+++ 05-express-first-app/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/detail', function(req, res, next) {
+  res.send('detail');
+});
+
 module.exports = router;
