49ae36c05d7813857170ce4596587d92764ebaf9
Vasant Hugar <vasant.hugar@indpro.se>
2018-10-31 15:40:23 +0530

    Deleted Users.js

--- 05-express-first-app/routes/index.js
+++ 05-express-first-app/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/users/detail', function(req, res, next) {
+  res.send('detail');
+});
+
 module.exports = router;
