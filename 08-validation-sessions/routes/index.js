48600e2687e793d9252020dbcbcf840a1b44fe93
Vasant Hugar <vasant.hugar@indpro.se>
2018-11-20 16:37:52 +0530

    Added Session and Validator with Tiny form and error message

--- 08-validation-sessions/routes/index.js
+++ 08-validation-sessions/routes/index.js
@@ -3,16 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'GET and POST' });
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
 });
 
-router.get('/test/:id', function (req, res, nex) {
-  res.render('test', { output: req.params.id });
-});
+router.post('/submit', function (req, res, next) {
+  // Check Validaity
+  req.check('email', 'Invalid email address').isEmail(); // Here email should be same as in "index.hbs" name of email field.
+  req.check('password', 'Password is invalid').isLength({ min: 4 }).equals(req.body.confirmPassword);
 
-router.post('/test/submit', function (req, res, next) {
-  var id = req.body.id;
-  res.redirect('/test/' + id);
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
 });
 
 module.exports = router;
