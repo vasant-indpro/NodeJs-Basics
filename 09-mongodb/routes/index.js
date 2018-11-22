227eea0760ce356db8c9a0d35e4a41fa9d73f4b6
Vasant Hugar <vasant.hugar@indpro.se>
2018-11-22 11:02:17 +0530

    Insert into mongodb

--- 09-mongodb/routes/index.js
+++ 09-mongodb/routes/index.js
@@ -1,18 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const mongo = require('mongodb');
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'GET and POST' });
 });
 
-router.get('/test/:id', function (req, res, nex) {
-  res.render('test', { output: req.params.id });
-});
-
 router.post('/test/submit', function (req, res, next) {
-  var id = req.body.id;
-  res.redirect('/test/' + id);
+  var item = {
+    id: req.body.id
+  };
+
+  mongo.MongoClient.connect(url,{ useNewUrlParser: true }, function(error, db){
+    assert.equal(null, error);
+    var dbo = db.db("mydb");
+    var collection = dbo.collection('user-data');
+    
+    collection.insertOne(item, function(error, result) {
+      assert.equal(null, error);
+      console.log('Item Inserted!');
+      db.close();
+    });
+  });
 });
 
 module.exports = router;
