9bcc0783a6c01a2df719af6d0d334a8aefb5654b
Vasant Hugar <vasant.hugar@indpro.se>
2018-11-22 20:38:17 +0530

    insert Login: username and password
    fetch
    Show userlist: Username and Password

--- 09-mongodb/routes/index.js
+++ 09-mongodb/routes/index.js
@@ -7,23 +7,47 @@ const url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'GET and POST' });
+  res.render('index', { title: 'login' });
 });
 
-router.post('/test/submit', function (req, res, next) {
+router.post('/login', function (req, res, next) {
   var item = {
-    id: req.body.id
+    username: req.body.username,
+    password: req.body.password
   };
 
-  mongo.MongoClient.connect(url,{ useNewUrlParser: true }, function(error, db){
+  mongo.MongoClient.connect(url, { useNewUrlParser: true }, function (error, db) {
     assert.equal(null, error);
-    var dbo = db.db("mydb");
-    var collection = dbo.collection('user-data');
-    
-    collection.insertOne(item, function(error, result) {
+    var dbo = db.db("test");
+    var collection = dbo.collection('user-login');
+
+    collection.insertOne(item, function (error, result) {
       assert.equal(null, error);
       console.log('Item Inserted!');
+      console.log(item);
       db.close();
+
+      res.render('message', { title: 'Success!', message: 'Item inserted!' });
+    });
+  });
+});
+
+router.get('/userlist', function (req, res, next) {
+
+  var resultArray = [];
+  mongo.MongoClient.connect(url, { useNewUrlParser: true }, function (error, db) {
+    assert.equal(null, error);
+
+    var dbo = db.db("test");
+    var cursor = dbo.collection('user-login').find();
+
+    cursor.forEach(function (doc, error) {
+      assert.equal(null, error);
+      resultArray.push(doc);
+      console.log(resultArray);
+    }, function () {
+      db.close();
+      res.render('message', { array: resultArray });
     });
   });
 });
